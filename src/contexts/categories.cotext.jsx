2585fb309f17a8e74e09e5c3dd79d39d8fec143a
Brianna <briannaondris@brightlineinteractive.com>
2023-12-01 08:40:31 -0500

    Added dynamic routing and fetching firebase data correctly

---
+++ src/contexts/categories.cotext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useState, useEffect } from "react";
+import { getCategoriesAndDocuments } from "../utils/firebase.utils.jsx";
+
+import SHOP_DATA from '../shop-data.js';
+
+export const CategoriesContext = createContext({
+    categoriesMap: {},
+});
+
+export const CategoriesProvider = ({children}) => {
+    const [categoriesMap, setCategoriesMap] = useState({});
+    // Only use once to add data
+    // useEffect(() => {
+    //     addCollectionAndDocuments('categories', SHOP_DATA);
+    // }, [])
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categorymap = await getCategoriesAndDocuments();
+            console.log(categorymap)
+            setCategoriesMap(categorymap)
+        }
+
+        getCategoriesMap();
+    }, [])
+
+    const value = {categoriesMap};
+
+    return (
+        <CategoriesContext.Provider value={value} > {children} </CategoriesContext.Provider>
+    )
+}
